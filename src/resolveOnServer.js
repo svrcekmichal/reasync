139740bb55baf8980750c4fbb46e7e42f7f6e15a
Michal Svrček <svrcekmichal@gmail.com>
2016-02-20 12:47:51 +0100

    fixed issue with defer resolving not promised

--- src/resolveOnServer.js
+++ src/resolveOnServer.js
@@ -2,6 +2,7 @@ import {getPreResolveDependencies, getDeferResolveDependencies} from './helpers'
 
 export const resolveOnServer = ({location,params, components}, {getState,dispatch}, custom = {}) => {
   const attrs = {...custom, location, params, getState, dispatch};
+  const deferResolve = () => Promise.all(getDeferResolveDependencies(components)(attrs));
   return Promise.all(getPreResolveDependencies(components)(attrs))
-    .then(getDeferResolveDependencies(components)(attrs))
+    .then(deferResolve,deferResolve);
 }
